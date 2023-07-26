3b504d7d72f7f74bd365952dafebcb68476b112c
David Dal Busco <david.dalbusco@outlook.com>
2023-07-27 00:19:19 +0200

    feat: load identity

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
+import {HttpAgent} from "@dfinity/agent";
+import {SnsGovernanceCanister} from "@dfinity/sns";
+import {Principal} from "@dfinity/principal";
+import {AuthClient} from "@dfinity/auth-client";
+
 export {SnsGovernanceCanister} from '@dfinity/sns';
+
+const createAuthClient = (): Promise<AuthClient> =>
+    AuthClient.create({
+        idleOptions: {
+            disableIdle: true,
+            disableDefaultIdleCallback: true
+        }
+    });
+
+export const initSnsGovernanceCanister = async (canisterId :string) => {
+    const authClient = await createAuthClient()
+
+    const agent = new HttpAgent({
+        identity: authClient.getIdentity()
+    });
+
+    return SnsGovernanceCanister.create({
+        canisterId: Principal.fromText(canisterId),
+        agent
+    })
+}
