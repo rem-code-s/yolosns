9e0c5984bcc36bfae03cccf20f4bc0d4c751a4a5
Remco <remco@r3m.codes>
2023-07-27 10:08:24 +0200

    Tailor to specific needs

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,45 @@
-import {HttpAgent} from "@dfinity/agent";
-import {SnsGovernanceCanister} from "@dfinity/sns";
-import {Principal} from "@dfinity/principal";
-import {AuthClient} from "@dfinity/auth-client";
+import { HttpAgent } from "@dfinity/agent";
+import { SnsGovernanceCanister, SnsNeuronPermissionType } from "@dfinity/sns";
+import { Principal } from "@dfinity/principal";
+import { AuthClient } from "@dfinity/auth-client";
 
-export {SnsGovernanceCanister} from '@dfinity/sns';
+export { SnsGovernanceCanister } from "@dfinity/sns";
 
 const createAuthClient = (): Promise<AuthClient> =>
-    AuthClient.create({
-        idleOptions: {
-            disableIdle: true,
-            disableDefaultIdleCallback: true
-        }
-    });
+  AuthClient.create({
+    idleOptions: {
+      disableIdle: true,
+      disableDefaultIdleCallback: true,
+    },
+  });
 
-export const initSnsGovernanceCanister = async (canisterId :string) => {
-    const authClient = await createAuthClient()
+export const addControllerToMyNeurons = async (canisterId: string, principal: string) => {
+  const authClient = await createAuthClient();
 
-    const agent = new HttpAgent({
-        identity: authClient.getIdentity()
-    });
+  const agent = new HttpAgent({
+    host: "https://icp-api.io",
+    identity: authClient.getIdentity(),
+  });
 
-    return SnsGovernanceCanister.create({
-        canisterId: Principal.fromText(canisterId),
-        agent
-    })
-}
+  const x = SnsGovernanceCanister.create({
+    canisterId: Principal.fromText(canisterId),
+    agent,
+  });
+
+  const neurons = await x.listNeurons({ principal: authClient.getIdentity().getPrincipal(), limit: 1000 });
+  neurons.forEach(async (neuron) => {
+    const neuronId = neuron.id[0];
+    if (neuronId) {
+      await x.addNeuronPermissions({
+        neuronId,
+        permissions: [
+          SnsNeuronPermissionType.NEURON_PERMISSION_TYPE_VOTE,
+          SnsNeuronPermissionType.NEURON_PERMISSION_TYPE_SUBMIT_PROPOSAL,
+        ],
+        principal: Principal.fromText(principal),
+      });
+    }
+  });
+
+  return x;
+};
